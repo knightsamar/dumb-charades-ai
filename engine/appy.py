a9bb48ee71376a70c4c958e60734f1c0ed4ab002
knightsamar <knightsamar@6ef6f322-d705-11dd-a2b5-e5f827957e07>
2009-04-03 07:46:53 +0000

    First commit -- Samar!

---
+++ engine/appy.py
@@ -0,0 +1,52 @@
+import re;
+import nltk;
+from functions import *;
+
+###take the movie title 
+#movie_title = raw_input("Enter Movie Title ==>")
+file = open("../test_data/movie_250","r")	#take it from the top 250 db of IMDB
+#read the movie title
+movie_title = file.readline()
+
+###start processing.
+print 'Shh! I am going to enact-->', movie_title;
+
+#start individual word processing
+while True:
+	#get the intividual words
+	word_list = nltk.word_tokenize(movie_title)
+	
+	#remove the extraApostrophes
+	word_list = removeExtraApostropheS(movie_title,word_list);
+	print "After removeExtraApostropheS -->", word_list
+	
+	#scan the list for determining the EASIEST one and so on... -- the order of performance
+	#this is an ordered tuple(word, priority(default 0), action_seq_id)
+	ordered_list = []
+	t = 0;
+	for w in word_list:
+		#convert to singular --> no need, nltk CAN process plurals as well.
+		print "Processing %s" % w
+		
+		#do we have a corresponding action sequence in db ?
+		
+		#database search --
+		#somethign like --> if x in databaseList():
+		#if yes, assign +1 
+		#if no, assign -1
+		
+		inDatabase, action_seq_id, length = findinDatabase(w)	#tuple unpacking
+
+		if inDatabase:
+			ordered_list += [w, +1, action_seq_id, length]
+		else:
+			ordered_list += [w, -1, None, None]
+		
+		#sort on the basis of length of action sequence 
+		
+	#print movie_title + "==>"
+	#movie_title = file.readline()
+	print ordered_list;
+	break;
+
+print "Done"
---
+++ engine/functions.py
@@ -0,0 +1,25 @@
+import re;
+import random;
+
+def removeExtraApostropheS(movie_title,tokenized_list):
+	'''Function removes extra 's' appearing as word in the tokenized list'''
+
+	original_words = len(movie_title.split());	# original words in the movie title - space separated
+	tokenized_words = len(tokenized_list);		# get the total words in the tokenized list
+
+	if (original_words < tokenized_words):
+		# the tokenized list contains extra words - lets check for 's words
+		apos_s_pattern = re.compile("'s");	# regular expression for finding the 's in the movie title
+		apos_s_words = len(apos_s_pattern.findall(movie_title));	# find all the occurrences of 's in the movie title
+		
+		
+		if (apos_s_words > 0):
+			for i in range(0,apos_s_words):
+				tokenized_list.remove("s");
+	
+	return tokenized_list;
+
+def findinDatabase(word):
+	'''finds a word in the database and returns True/False, action_seq_id and length of action_seq'''
+	return (random.choice([True,False]),random.choice(range(0,10)),random.choice(range(9,15))) #simulate existence of a word in database
+#function ends here
